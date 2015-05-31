ec60b8c1a0cd414af04a5fbb593e903c8e9859b5
Sleepwalker <mail@asleepwalker.ru>
2015-05-31 21:22:34 +0300

    Added station dialog with streams

--- public/js/views/objects/station.js
+++ public/js/views/objects/station.js
@@ -1,10 +1,9 @@
-define(['underscore', 'backbone', 'leaflet'], function(_, Backbone, L) {
+define(['underscore', 'backbone', 'leaflet', 'views/dialog'], function(_, Backbone, L, DialogView) {
 	'use strict';
 
 	return Backbone.View.extend({
 		initialize: function(params) {
 			params.object.setIcon(this.markerIcon);
-			params.object.bindPopup(this.popupContent(params.data));
 			this.listenTo(params.object, 'click', this.onClick);
 
 			this.params = params;
@@ -14,11 +13,32 @@ define(['underscore', 'backbone', 'leaflet'], function(_, Backbone, L) {
 			iconSize: [50, 50],
 			iconAnchor: [0, 0]
 		}),
-		popupContent: function(data) {
-			return _.template($('#station-popup').html())(data);
+		dialogContent: function(data) {
+			return _.template($('#station-dialog').html())(data);
 		},
 		onClick: function() {
-			this.params.object.openPopup();
+			var dialog = new DialogView(this.params.data.caption, $body),
+				$body = $(this.dialogContent(this.params.data));
+
+			if (typeof(this.params.data.list) !== 'undefined') {
+				var $list = $body.find('.streams-list'),
+					app = this.params.appModel;
+
+				_.each(this.params.data.list, function(item) {
+					$('<li>')
+						.html(item.caption)
+						.click(function() {
+							app.load(item.region, item.map, { x: item.x, y: item.y });
+							dialog.close();
+						})
+						.appendTo($list);
+				});
+			}
+
+			dialog
+				.setCaption(this.params.data.caption)
+				.setContent($body)
+				.open();
 		}
 	});
 
