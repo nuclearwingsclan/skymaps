e0795de53bd39d69388f1c4ad93b639f28b2bdf6
Sleepwalker <mail@asleepwalker.ru>
2015-05-31 21:21:03 +0300

    Added dialog interface, styles for popups and dialogs

---
+++ public/js/views/dialog.js
@@ -0,0 +1,39 @@
+define(['jquery', 'underscore', 'backbone', 'leaflet'], function($, _, Backbone, L) {
+	'use strict';
+
+	return function(options) {
+		var $dialog = $('<div class="dialog">'),
+			$container = $('<div class="dialog-container">').appendTo($dialog),
+			$caption = $('<div class="dialog-caption">').appendTo($container),
+			$body = $('<div class="dialog-body">').appendTo($container),
+			$closeButton = $('<button class="dialog-close-button">').html('&times;').appendTo($container);
+
+		this.setCaption = function(newValue) {
+			$caption.text(newValue);
+			return this;
+		};
+
+		this.setContent = function(body) {
+			if (typeof body == 'object' && body instanceof $) {
+				$body.append(body);
+			} else {
+				$body.html(body);
+			}
+			return this;
+		};
+
+		this.open = function() {
+			$dialog.appendTo('body');
+			return this;
+		};
+
+		this.close = function() {
+			$dialog.remove();
+		};
+
+		$closeButton.click(function() {
+			$dialog.remove();
+		});
+	};
+
+});
