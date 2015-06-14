0857fd0eab46402c33046ed8cb24eaf82228d8e1
Sleepwalker <mail@asleepwalker.ru>
2015-06-14 18:36:12 +0300

    Added barrier object

---
+++ public/js/views/objects/barrier.js
@@ -0,0 +1,12 @@
+define(['underscore', 'backbone', 'leaflet', 'views/objects/flag'], function(_, Backbone, L, FlagView) {
+	'use strict';
+
+	return FlagView.extend({
+		markerIcon: L.icon({
+			iconUrl: '/i/objects/barrier.svg',
+			iconSize: [20, 20],
+			iconAnchor: [4, 4]
+		})
+	});
+
+});
