6554394b872c6571fc189b09ac2e69ecad7dc765
Sleepwalker <mail@asleepwalker.ru>
2015-06-14 20:11:15 +0300

    Changed default map index

--- public/js/router.js
+++ public/js/router.js
@@ -11,14 +11,16 @@ define(['backbone'], function(Backbone) {
 			':region/:map/': 'loadMap'
 		},
 		index: function() {
-			this.loadMap('pf', 'index');
+			this.loadMap('index', 'index');
 		},
 		loadMap: function(region, level) {
 			this.appModel.load(region, level);
 		},
 		navigate: function() {
 			var location = this.appModel.get('location');
-			Backbone.history.navigate(location.region + '/' + location.level + '/');
+			if (location.region != 'index') {
+				Backbone.history.navigate(location.region + '/' + location.level + '/');
+			}
 		}
 	});
 
