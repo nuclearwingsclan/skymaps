34acd14708e164fd2f6b0c22acb5837a22927d57
Sleepwalker <mail@asleepwalker.ru>
2016-08-06 23:29:45 +0300

    Add open editor control

--- src/js/views/controls.js
+++ src/js/views/controls.js
@@ -1,22 +1,31 @@
 define(['underscore', 'backbone', 'jquery'], function(_, Backbone, $) {
 	'use strict';
 
+	var $editBtn = $('.controls button.edit');
+
 	var $navigatorBtn = $('.caption .controls button.navigator, button.open-navigator');
 	var $navigatorOverlay = $('.navigator-overlay');
 
 	return Backbone.View.extend({
-		initialize: function() {
+		initialize: function(options) {
+			this.app = options.app;
+
 			$navigatorBtn.click(_.bind(this.toggleNavigator, this));
 			$navigatorOverlay.on('click mousedown touchdown', _.bind(this.closeNavigator, this));
+
+			this.listenTo(this.app, 'change:location', this.updateEditorLink);
+		},
+
+		updateEditorLink: function() {
+			var location = this.app.get('location');
+			$editBtn.attr('onclick', 'window.open("/editor/?map=' + location.region + '/' + location.level + '")');
 		},
 
 		toggleNavigator: function() {
-			console.log('toggle');
 			$('body').toggleClass('navigator');
 		},
 
 		closeNavigator: function() {
-			console.log('cloes');
 			$('body').removeClass('navigator');
 		}		
 	});
